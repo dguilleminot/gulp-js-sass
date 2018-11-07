9f66b50c234f359925bccd8fb90f4c6a9d9feb8c
Dorian <dorian.guilleminot@gmail.com>
2018-11-07 13:38:08 +0100

    [FIX] entry point

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,12 @@ gulp
       .pipe(babel({
           presets: ['env']
       }))
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('./'));
   })
 
   .task('test', ['build'], () => {
     // make sure it built before imorting
-    const scssJs = require('./dist/index.js');
+    const scssJs = require('./index.js');
 
     return gulp.src('test/**/*.scss')
       .pipe(concat('main.css'))
